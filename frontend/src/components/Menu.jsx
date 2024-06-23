df241bca043a85c4d35fc47e0001f95656ea01de
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-06-23 00:26:48 -0400

    changes

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,68 +1,32 @@
 import { useState, useEffect } from 'react';
+import "./Menu.css";
 
 
-function Menus() {
-  const [menu, setMenu] = useState([]);
-  const [error, setError] = useState([]);
-
-//   const BASE_URL = 'http://localhost:3000/menus';
-
-  useEffect(() => {
-    fetch('/api/menus')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(menu => setMenu(menu))
-      .catch(error => {
-        console.error('Error fetching the data:', error);
-        setError(error);
-      });
-  }, []);
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  if (!menu) {
-    return <div>Loading...</div>;
-  }
-
-
-
-
-
-//   useEffect(() => {
-//     const getMenu = async () => {
-//       try {
-//         const response = await fetch("/menus");
-// console.log(response)
-//         if (response.status !== 200) {
-//           return;
-//         }
-
-//         const data = await response.json();
-// console.log(data)
-//         setMenu(data);
-//       } catch (err) {
-//         console.log(err);
-//       }
-//     };
-
-//     getMenu();
-
-//   }, []);
-
-return (
-    <div>
-      <h3>{menu.name}</h3>
-      <img src={menu.image} style={{height: "200px", width: "200px"}}  />
-      <h4>{menu.description}</h4>
-      <h4>${menu.price}</h4>
-    </div>
-    );
-  }
+const Menu = () => {
+    const [menus, setMenus] = useState([]);
+    const BASE_URL = 'https://backend-capstone-6-moig.onrender.com';
   
-  export default Menus;
+    useEffect(() => {
+      fetch(`${BASE_URL}/menus`)
+        .then(response => response.json())
+        .then(data => setMenus(data))
+        .catch(error => console.error('Error fetching menus:', error));
+    }, []);
+  
+    return (
+      <div className="mainMenu">
+        <h2>Menu</h2>
+          {menus.map(menu => (
+           <div key={menu._id} className="menu">
+           <h3>{menu.name}</h3>
+           <img src={menu.image} style={{height: "200px", width: "200px"}}  />
+           <h4>{menu.description}</h4>
+           <h4>${menu.price}</h4>
+            </div>
+          ))}
+      </div>
+    );
+  };
+  
+  export default Menu;
+  
