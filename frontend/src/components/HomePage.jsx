085e5ab12105e242e94d0c9e69e1f3782dc4e311
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-06-25 15:25:22 -0400

    added some implementations

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -5,13 +5,19 @@ const HomePage = () => {
     <div className='homepage'>
       <h1>Welcome to AFOOD Restaurant</h1>
       <p className='enjoy'>Enjoy our delicious food!</p>
-      <img src="https://previews.123rf.com/images/didecs/didecs1504/didecs150400007/38487573-old-recipe-note-background-concept.jpg" />
+      <img
+       src="https://previews.123rf.com/images/didecs/didecs1504/didecs150400007/38487573-old-recipe-note-background-concept.jpg" />
+     
       <div className='home-btn'>
       <a href="/register">Registration</a>
       <a href="/newOrder">Place Order</a>
       </div>
+      <img src='../images/anime.jpg' 
+       alt="Moving Image"
+       className='mocing-image' />
       
     </div>
+    
   );
 };
 
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -24,10 +24,10 @@ const NavBar = ({orderCount}) => {
           <Link className="nav-link" to="/menu">Menu</Link>
         </li>
         <li className="nav-item" >
-          <Link className="nav-link" to="/order">Orders</Link>
+          <Link className="nav-link" to="/order">Order List</Link>
         </li>
         <li className="nav-item" >
-          <Link className="nav-link" to="/user">Users </Link>
+          <Link className="nav-link" to="/user">User</Link>
         </li>
         <li className="nav-item" >
           <Link className="nav-link" to="/about">About Us</Link>
